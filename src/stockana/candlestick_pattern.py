8b541d17e3c444b0098cfe66818dc0275da378be
PenHsuanWang <willy118116@gmail.com>
2023-12-08 19:18:34 +0800

    fix date indexing problem

--- src/stockana/candlestick_pattern.py
+++ src/stockana/candlestick_pattern.py
@@ -383,37 +383,39 @@ class PatternRecognizer:
             prev_prev_day = self.data.iloc[i - 2]
             days = self.data.iloc[i - 2:i + 1]
 
+            date_index = day.name
+
             if PatternDefinitions.is_bullish_engulfing(day, prev_day):
-                self.data.at[i, 'Pattern'] = 'Bullish Engulfing'
+                self.data.at[date_index, 'Pattern'] = 'Bullish Engulfing'
             elif PatternDefinitions.is_bearish_engulfing(day, prev_day):
-                self.data.at[i, 'Pattern'] = 'Bearish Engulfing'
+                self.data.at[date_index, 'Pattern'] = 'Bearish Engulfing'
             elif PatternDefinitions.is_morning_star(days):
-                self.data.at[i, 'Pattern'] = 'Morning Star'
+                self.data.at[date_index, 'Pattern'] = 'Morning Star'
             elif PatternDefinitions.is_evening_star(days):
-                self.data.at[i, 'Pattern'] = 'Evening Star'
+                self.data.at[date_index, 'Pattern'] = 'Evening Star'
             elif PatternDefinitions.is_hammer(day, is_downtrend=self.is_downtrend(i)):
-                self.data.at[i, 'Pattern'] = 'Hammer'
+                self.data.at[date_index, 'Pattern'] = 'Hammer'
             elif PatternDefinitions.is_inverse_hammer(day):
-                self.data.at[i, 'Pattern'] = 'Inverse Hammer'
+                self.data.at[date_index, 'Pattern'] = 'Inverse Hammer'
             elif PatternDefinitions.is_hanging_man(day, prev_day, is_uptrend=self.is_uptrend(i)):
-                self.data.at[i, 'Pattern'] = 'Hanging Man'
+                self.data.at[date_index, 'Pattern'] = 'Hanging Man'
             elif PatternDefinitions.is_shooting_star(day, prev_day, is_uptrend=self.is_uptrend(i)):
-                self.data.at[i, 'Pattern'] = 'Shooting Star'
+                self.data.at[date_index, 'Pattern'] = 'Shooting Star'
             elif PatternDefinitions.is_three_white_soldiers(days):
-                self.data.at[i, 'Pattern'] = 'Three White Soldiers'
+                self.data.at[date_index, 'Pattern'] = 'Three White Soldiers'
             elif PatternDefinitions.is_three_black_crows(days):
-                self.data.at[i, 'Pattern'] = 'Three Black Crows'
+                self.data.at[date_index, 'Pattern'] = 'Three Black Crows'
             elif PatternDefinitions.is_dark_cloud_cover(day, prev_day):
-                self.data.at[i, 'Pattern'] = 'Dark Cloud Cover'
+                self.data.at[date_index, 'Pattern'] = 'Dark Cloud Cover'
             elif PatternDefinitions.is_engulfing_with_volume(day, prev_day, 'bullish'):
-                self.data.at[i, 'Pattern'] = 'Bullish Engulfing with Volume'
+                self.data.at[date_index, 'Pattern'] = 'Bullish Engulfing with Volume'
             elif PatternDefinitions.is_engulfing_with_volume(day, prev_day, 'bearish'):
-                self.data.at[i, 'Pattern'] = 'Bearish Engulfing with Volume'
+                self.data.at[date_index, 'Pattern'] = 'Bearish Engulfing with Volume'
             elif PatternDefinitions.is_doji_with_volume(day, prev_day, 'standard'):
-                self.data.at[i, 'Pattern'] = 'Doji with Volume'
+                self.data.at[date_index, 'Pattern'] = 'Doji with Volume'
             elif PatternDefinitions.is_hammer_with_volume(day, prev_day, is_downtrend=self.is_downtrend(i)):
-                self.data.at[i, 'Pattern'] = 'Hammer with Volume'
+                self.data.at[date_index, 'Pattern'] = 'Hammer with Volume'
             elif PatternDefinitions.is_shooting_star_with_volume(day, prev_day, is_uptrend=self.is_uptrend(i)):
-                self.data.at[i, 'Pattern'] = 'Shooting Star with Volume'
+                self.data.at[date_index, 'Pattern'] = 'Shooting Star with Volume'
 
         return self.data
